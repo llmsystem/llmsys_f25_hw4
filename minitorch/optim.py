0c4627b5709dece9c75ac13f919780973bd8f143
yubol <yubol@andrew.cmu.edu>
2026-02-18 17:07:00 -0500

    Fix Adam second-moment beta coefficient

--- minitorch/optim.py
+++ minitorch/optim.py
@@ -65,7 +65,7 @@ class Adam(Optimizer):
 
                     state['step'] += 1
                     state['exp_avg'] = state['exp_avg'] * self.beta1 + (1 - self.beta1) * grad
-                    state['exp_avg_sq'] = state['exp_avg_sq'] * self.beta2 + (1 - self.beta1) * grad ** 2
+                    state['exp_avg_sq'] = state['exp_avg_sq'] * self.beta2 + (1 - self.beta2) * grad ** 2
 
                     # denom = exp_avg_sq.sqrt().add_(group['eps'])
                     denom = state['exp_avg_sq'] ** 0.5 + self.eps
