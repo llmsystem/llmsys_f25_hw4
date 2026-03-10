93a16efd38a03fdd2978c1d4b49ef87e9d39c6d5
Saltfish <64593412+codycjy@users.noreply.github.com>
2026-03-10 19:31:19 -0400

    fix: params parse conflict

--- project/run_machine_translation.py
+++ project/run_machine_translation.py
@@ -7,9 +7,6 @@ import json
 import random
 import datasets
 import numpy as np
-import argparse
-from distutils.util import strtobool
-
 from sacrebleu.metrics import BLEU
 from transformers import AutoTokenizer
 from tokenizers import ByteLevelBPETokenizer
@@ -188,14 +185,6 @@ def train(model, optimizer, examples, n_samples, collate_fn, batch_size, desc):
             lr=optimizer.lr)
 
 
-def parse_args():
-    def str2bool(x):
-        return bool(strtobool(x))
-        
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--use-fused-kernel', type=str2bool, default=False)
-    return parser.parse_args()
-
 
 def main(dataset_name='bbaaaa/iwslt14-de-en-preprocess',
          model_max_length=40,
@@ -205,8 +194,8 @@ def main(dataset_name='bbaaaa/iwslt14-de-en-preprocess',
          samples_per_epoch=20000,
          n_vocab=10000,
          n_embd=256,
-         seed=11111):
-    args = parse_args()
+         seed=11111,
+         use_fused_kernel=False):
              
     np.random.seed(seed)
     random.seed(seed)
@@ -225,7 +214,7 @@ def main(dataset_name='bbaaaa/iwslt14-de-en-preprocess',
         'p_dropout'   : 0.1,  # x_pdrop
         'ln_eps'      : 1e-5, # layer_norm_epsilon
         'backend'     : backend,
-        'use_fused_kernel': args.use_fused_kernel
+        'use_fused_kernel': use_fused_kernel
     }
 
     model = DecoderLM(**config)
